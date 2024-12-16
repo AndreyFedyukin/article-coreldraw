9f5ba175b8884a190a2f312e8f1e8fadfdd52cda
Andrey <81571422+AndreyFedyukin@users.noreply.github.com>
2024-12-16 16:17:09 +0300

    add new files

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// Бегущая строка skills ***********************************************
+const scrollers = document.querySelectorAll(".scroller");
+
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    scroller.setAttribute("data-animated", true);
+
+    const scrollerInner = scroller.querySelector(".images-list");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
